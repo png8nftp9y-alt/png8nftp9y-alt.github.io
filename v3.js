0191f4b83b16e66f9e3fab9345f8252eebe6fd75
png8nftp9y-alt <png8nftp9y@privaterelay.appleid.com>
2026-08-13 22:50:18 +0200

    Emergency restore Court Watch v3 backup data source

--- v3.js
+++ v3.js
@@ -1,4 +1,4 @@
-const V3='https://raw.githubusercontent.com/png8nftp9y-alt/png8nftp9y-alt.github.io/main/dist/v3/';
+const V3='https://raw.githubusercontent.com/png8nftp9y-alt/png8nftp9y-alt.github.io/55ff89a05c6bdff332045935be79cf49c40c9cbc/dist/v3/';
 const FORMER_PLAYERS=new Set(['martina-busa','manuel-natale','pietro-sala','niccolo-zanaga']);
 const state={data:null,month:new Date(),agenda:new Date(),selected:new Set(),openPicker:null};
 const $=id=>document.getElementById(id),esc=s=>String(s??'').replace(/[&<>"]/g,c=>({'&':'&amp;','<':'&lt;','>':'&gt;','"':'&quot;'}[c]));
